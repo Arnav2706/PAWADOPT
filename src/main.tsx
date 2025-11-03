24ad13d9d3f435ffc1fa2fbf611af86ec0c6f055
Arnav Kumar <arnavkumarpatna@gmail.com>
2025-11-03 23:10:14 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { CartProvider } from "./hooks/useCart";
+import { CartProvider, FavoritesProvider } from "./hooks/useCart";
+
 createRoot(document.getElementById("root")!).render(
   <CartProvider>
-    <App />
+    <FavoritesProvider>
+      <App />
+    </FavoritesProvider>
   </CartProvider>
 );
